a25b98728c8a3a8b012b1827446fc6020b916e88
Cheredn1koff <cheredn1koff@yandex.ru>
2024-10-16 12:49:24 +0300

    code according to PEP8

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,20 +1,16 @@
 ﻿import json
+from django.core.management.base import BaseCommand
 
-from django.core.management import BaseCommand
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Загрузка ингредиентов в БД'
-
     def handle(self, *args, **options):
-        with open(
-                './data/ingredients.json',
-                encoding='utf-8'
-        ) as data:
-            for row in json.load(data):
-                ingredient = Ingredient(
-                    name=row['name'].capitalize(),
-                    measurement_unit=row['measurement_unit']
-                )
-                ingredient.save()
+        self.load_ingredient()
+
+    def load_ingredient(self, *args, **kwargs):
+        with open('data/ingredients.json',
+                  newline='', encoding='utf-8') as f_ingredient:
+            ingredients = json.loads(f_ingredient.read())
+            for ingredient in ingredients:
+                Ingredient.objects.get_or_create(**ingredient)
