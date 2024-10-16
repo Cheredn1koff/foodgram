f5e42c988a3e12d825ae98884994a55567e4d128
Cheredn1koff <cheredn1koff@yandex.ru>
2024-10-16 21:51:59 +0300

    code according to PEP8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -140,6 +140,10 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'api.pagination.PageLimitPagination',
